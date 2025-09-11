ca75319a3457ca9669e247d2364a9e3047fe3ca4
Creamcheesepie <gnldbs1004@naver.com>
2025-09-11 15:38:31 +0900

    중간 점검 커밋

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+           <div>this is posts page </div>
+           <div>fuxkxng codepen, IDE is good.</div>
+           <div>here's speed is so fast</div>
+        </>
+   
+    );
+  }
+  
