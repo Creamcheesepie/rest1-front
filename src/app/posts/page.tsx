8eaf46e4f31d365d352fc79691e234a77247f5d1
Creamcheesepie <gnldbs1004@naver.com>
2025-09-12 11:39:25 +0900

    Link 사용으로 링크 이동 최적화.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 export default function Home() {
     return (
         <>
            <div>this is posts page </div>
            <div>fuxkxng codepen, IDE is good.</div>
-           <div>here's speed is so fast</div>
+           <div>here's latency is so low, cool.</div>
+           <div>I'll admire the developer who use the VIM</div>
         </>
    
     );
