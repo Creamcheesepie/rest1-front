75d1ada507cb11e1c65a5d6fe8b6236abf6d5c3c
Creamcheesepie <gnldbs1004@naver.com>
2025-09-12 11:49:42 +0900

    테일윈드 앞에 ! 붙일시 다른 속성에 의해 무시되는 것을 방지하고 해당 속성을 강조할 수 있다

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
     return (
         <>
            <div>this is posts page </div>
-           <div>fuxkxng codepen, IDE is good.</div>
-           <div>here's latency is so low, cool.</div>
-           <div>I'll admire the developer who use the VIM</div>
+           
         </>
    
     );
