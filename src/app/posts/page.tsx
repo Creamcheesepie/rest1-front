dc5b001172534e724d71133f5b11b1e43d6f2176
Creamcheesepie <gnldbs1004@naver.com>
2025-09-12 14:12:20 +0900

    값을 가져와서 렌더링하는 시점까지의 지연시간 동안 loading 표시 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-    const [posts, setPosts]  = useState([]);
+    const [posts, setPosts]  = useState<{id: number, title: string}[]>([]);
 
     useEffect(() => {
         const posts = fetch("http://localhost:8080/api/v1/posts")
@@ -17,15 +17,19 @@ export default function Home() {
    
 
     return (
+  
         <div className="flex flex-col gap 6">
             <h1>글 목록</h1>
+            {posts.length === 0 && <div>Loading...</div>}
+            { posts.length > 0 && (
             <ul>
-                {posts.map((post : {id: number, title: string}) =>( // 명시가 가능하다.
+                {posts.map((post) =>( // 명시가 가능하다.
                     <li key={post.id}>
                         {post.id} : {post.title}
                     </li>
                 ))}
             </ul>
+            )}
         </div>
    
     );
