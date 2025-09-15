f8994bc70bcc770ca066f2f21e49db8c7b6ff40c
Creamcheesepie <gnldbs1004@naver.com>
2025-09-15 10:32:53 +0900

    글 작성 후 상세페이지 이동 및, 해당 상세페이지에서 뒤로가기 누를 시 글 목록으로 이동되게 수정

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -3,9 +3,12 @@
 import { fetchApi } from "@/lib/client";
 import { PostDto } from "@/type/post";
 import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+    
+    const router = useRouter();
 
     const handleSubmit = (e : any) =>{
         e.preventDefault();
@@ -34,6 +37,7 @@ export default function Home() {
         })
         .then((data) => {
             alert(data.msg);
+            router.replace(`/posts/${data.data.postDto.id}`);
         });
     };
 
@@ -42,9 +46,12 @@ export default function Home() {
         <>
             <h1>새 글 작성</h1>
             <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
-                <input type="text" name="title" placeholder="제목"></input>
-                <input type="text" name="content" placeholder="내용"></input>
-                <button type="submit">저장</button>
+                <input className="border border-gray-3000 rounded p-2"
+                 type="text" name="title" placeholder="제목"/>
+                <input className="border border-gray-3000 rounded p-2 h-20" 
+                type="text" name="content" placeholder="내용"/>
+                <button  className="border border-gray-3000 rounded p-2"
+                type="submit">저장</button>
             </form>
         </>
     );
